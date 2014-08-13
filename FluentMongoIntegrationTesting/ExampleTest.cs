96db077dc33481268d178a594053894e69b01f1f
Lawrence Gripper <lawrencegripper@hotmail.co.uk>
2014-08-13 10:53:54 +0100

    First commit of mongo integration testing example

---
+++ FluentMongoIntegrationTesting/ExampleTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Embedded;
+using MongoDB.Driver;
+
+namespace FluentMongoIntegrationTesting
+{
+    [TestClass]
+    public class ExampleTestClass
+    {
+        private static EmbeddedMongoDbServer mongoEmbedded;
+        private static MongoClient mongoClient;
+        private static MongoServer mongoServer;
+
+        [ClassInitialize]
+        public static void StartMongoEmbedded(TestContext cont)
+        {
+            mongoEmbedded = new EmbeddedMongoDbServer();
+            mongoClient = mongoEmbedded.Client;
+            mongoServer = mongoClient.GetServer();
+
+        }
+
+        [ClassCleanup]
+        public static void ShutdownMongo()
+        {
+            mongoEmbedded.Dispose();
+        }
+
+        [TestInitialize]
+        public void CleanMongo()
+        {
+            var databases = mongoServer.GetDatabaseNames();
+            foreach (var databaseName in databases)
+            {
+                mongoServer.DropDatabase(databaseName);
+            }
+        }
+
+        [TestMethod]
+        public void ExampleTest()
+        {
+            //arrange
+            var databaseName = "testDatabase";
+            var collectionName = typeof(ExampleType).Name;
+            var db = mongoServer.GetDatabase(databaseName);
+            var collection = db.GetCollection<ExampleType>(collectionName);
+            
+            //act
+            var item = new ExampleType() { MyProperty = 1 };
+            collection.Insert(item);
+
+            //assert
+            mongoServer.AssertDatabaseExists(databaseName)
+                .AssertCollectionExists<ExampleType>()
+                .AssertCollectionItemCount(1)
+                .AssertCollectionHasItemWithProperty<ExampleType>(item, x => x.MyProperty == 1);
+        }
+
+        public class ExampleType
+        {
+            public ObjectId Id { get; set; }
+            public int MyProperty { get; set; }
+        }
+
+    }
+}
---
+++ FluentMongoIntegrationTesting/FluentMongoAssertions.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentMongoIntegrationTesting
+{
+    public static class FluentMongoAssertions
+    {
+        public static MongoDatabase AssertDatabaseExists(this MongoServer mongoServer, string databaseName)
+        {
+            var container = mongoServer.DatabaseExists(databaseName);
+            if (!container)
+            {
+                Assert.Fail(string.Format("Database doesn't exist '{0}'", databaseName));
+            }
+
+            Trace.WriteLine(string.Format("Database exists '{0}'", databaseName));
+
+            return mongoServer.GetDatabase(databaseName);
+        }
+
+        public static MongoCollection<T> AssertCollectionExists<T>(this MongoDatabase mongoDatabase)
+        {
+            var collectionName = typeof(T).Name;
+            if (!mongoDatabase.CollectionExists(collectionName))
+            {
+                Assert.Fail(string.Format("Collection doesn't exist '{0}'", collectionName));
+            }
+
+            Trace.WriteLine(string.Format("Collection exists '{0}'", collectionName));
+
+            return mongoDatabase.GetCollection<T>(collectionName);
+        }
+
+        public static MongoCollection<T> AssertCollectionItemCount<T>(this MongoCollection<T> mongoCollection, int count)
+        {
+            if (mongoCollection.Count() != count)
+            {
+                Assert.Fail(string.Format("Collection doesn't have correct count expected '{0}' actual '{1}'", count, mongoCollection.Count()));
+            }
+
+            Trace.WriteLine(string.Format("Collection had expected items count '{0}'", mongoCollection.Count()));
+
+            return mongoCollection;
+        }
+
+
+        public static MongoCollection<T> AssertCollectionHasItemWithProperty<T>(this MongoCollection<T> mongoCollection, T item, Expression<Func<T, bool>> itemSelector, int expectNumberOfItems = 1)
+        {
+            var queryable = mongoCollection.AsQueryable<T>();
+            var output = queryable.Where(itemSelector);
+
+            if (output.Count() != expectNumberOfItems)
+            {
+                Assert.Fail(string.Format("Collection doesn't have '{0}' items '{1}' in collection '{2}' when selected with '{3}'",expectNumberOfItems, typeof(T).Name, mongoCollection.Name, itemSelector.Body.ToString()));
+
+            }
+
+            Trace.WriteLine(string.Format("Collection has '{0}' items '{1}' in collection '{2}' selected with '{3}'",expectNumberOfItems, typeof(T).Name, mongoCollection.Name, itemSelector.Body.ToString()));
+
+            return mongoCollection;
+        }
+    }
+}
